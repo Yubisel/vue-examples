44a53df98e4001d82447e1f8f012e5db6818dc4d
Yubisel <yubiselv@gmail.com>
2019-12-22 19:08:59 -0300

    terminado el crud con localstorage

--- js/crud-localstorage.js
+++ js/crud-localstorage.js
@@ -3,42 +3,36 @@ var app = new Vue({
     data: {
         title: 'CRUD localStorage example',
         taskName: '',
-        tasks: [{
-                name: 'Apple',
-                cant: 15
-            },
-            {
-                name: 'Orange',
-                cant: 10
-            },
-            {
-                name: 'Pinneaple',
-                cant: 18
-            },
-            {
-                name: 'Pear',
-                cant: 0
-            }
-        ]
+        tasks: []
     },
     methods: {
         addTask() {
             if (this.taskName !== '') {
                 this.tasks.push({
                     name: this.taskName,
-                    cant: 0
+                    status: false
                 });
             }
             this.taskName = '';
+            localStorage.setItem('task-list', JSON.stringify(this.tasks));
+        },
+        completeTask(index){
+            this.tasks[index].status = true;
+            localStorage.setItem('task-list', JSON.stringify(this.tasks));
+        },
+        deleteTask(index){
+            this.tasks.splice(index, 1);
+            localStorage.setItem('task-list', JSON.stringify(this.tasks));
+        }
+    },
+    created(){
+        let taskDB = JSON.parse(localStorage.getItem('task-list'));
+        if (taskDB === null){
+            this.tasks = [];
+        }else{
+            this.tasks = taskDB;
         }
     },
     computed: {
-        // calcTotal() {
-        //     let total = 0;
-        //     for (const fruit of this.fruits) {
-        //         total += fruit.cant;
-        //     }
-        //     return total;
-        // }
     },
 });
